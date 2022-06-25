0d525009f8b6543e66904e6ae70054b16c73e1d9
Mariya Efremova <106958459+MariyaEfremova@users.noreply.github.com>
2022-06-25 12:08:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let addPoint = document.querySelector('.newPoint');
-let doings = document.querySelector('.doings')
+let doings = document.querySelector('.doings');
 
 let todoList = [];
+
+// let deleteArr = [];
 
 if (localStorage.getItem('doings')) {
     todoList = JSON.parse(localStorage.getItem('doings'));
@@ -21,6 +23,12 @@ document.addEventListener('keypress', function (event) {
         }
 
         displayPoints();
+
+        // let removeButton = document.getElementById(`${i}`);
+        // console.log(removeButton);
+        // deleteArr.push(removeButton);
+        // console.log(deleteArr);
+
         localStorage.setItem('doings', JSON.stringify(todoList));
     }
 });
@@ -31,21 +39,38 @@ function displayPoints() {
         displayPoint += `
         <li>
         <div class = "circle"></div>
+
         <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
-        <label for='item_${i}'>${item.todo}</label>
+        
+        <label for='item_${i}'>${item.todo}</label>  
+        <input type='image' id=${i} class='removeButton' src='../img/pngwing.png'>
         </li>
         `;
         doings.innerHTML = displayPoint;
+
+        // let removeButton = document.getElementById(`${i}`);
+        // console.log(removeButton);
+
+        // if (deleteArr.length == `${i}`) {
+        //     deleteArr.push(removeButton);
+        //     console.log(deleteArr);
+        // }
 
         document.getElementById('input_point').value = ""; //очистка поля ввода после нажатия Enter
     })
 }
 
+// изменение атрибута checked;
+// навешиваем событие 'change' на элементы списка .doings;
 doings.addEventListener('change', function (e) {
+    // получаем значение атрибута for элемента, на котором произошло событие;
     let idInput = e.target.getAttribute('id');
+    // получаем элемент со значением атрибута for, найденным выше; 
     let forLabel = doings.querySelector('[for=' + idInput + ']');
+    // получаем содержимое элемента, полученного выше;
     let valueLabel = forLabel.innerHTML;
 
+    // перебираем массив todoList;
     todoList.forEach(function (item) {
         if (item.todo === valueLabel) {
             item.checked = !item.checked;
@@ -53,3 +78,64 @@ doings.addEventListener('change', function (e) {
         }
     });
 });
+
+// deleteArr.forEach(function (item) {
+//     console.log(item);
+//     item.addEventListener('click', function (e) {
+//         // получаем содержимое label(пункт списка дел);
+//         let targetParent = e.target.previousElementSibling.innerHTML;
+//         // перебор массива todoList;
+//         todoList.forEach(function (item) {
+//             // если содержимое св-ва todo данного элемента массива = содержимому
+//             // label, то получаем индекс данного элемента и удаляем его;
+//             if (item.todo === targetParent) {
+//                 // удаляем весь элемент списка со страницы;
+//                 e.target.parentElement.remove();
+
+//                 let index = todoList.indexOf(item)
+//                 todoList.splice([index], 1);
+
+//                 // перезаписываем localStorage;
+//                 localStorage.setItem('doings', JSON.stringify(todoList));
+//             }
+//         });
+
+//     });
+// });
+
+// function deleteElement() {
+// получаем коллекцию кнопок удаления элемента списка;
+// let removeButton = document.getElementsByClassName('removeButton');
+// console.log(removeButton);
+// // }
+
+let removeButton = document.getElementsByClassName('removeButton');
+console.log(removeButton);
+
+// перебор полученной коллекции кнопок;
+for (let itemCollection of removeButton) {
+    // прослушка на элементы коллекции кнопок;
+    itemCollection.addEventListener('click', function (e) {
+        // получаем содержимое label(пункт списка дел);
+        let targetParent = e.target.previousElementSibling.innerHTML;
+        // перебор массива todoList;
+        todoList.forEach(function (item) {
+            // если содержимое св-ва todo данного элемента массива = содержимому
+            // label, то получаем индекс данного элемента и удаляем его;
+            if (item.todo === targetParent) {
+                // удаляем весь элемент списка со страницы;
+                e.target.parentElement.remove();
+
+                let index = todoList.indexOf(item)
+                todoList.splice([index], 1);
+
+                // перезаписываем localStorage;
+                localStorage.setItem('doings', JSON.stringify(todoList));
+            }
+        });
+
+    });
+}
+
+// createElement().then(deleteElement());
+// createElement().then(displayPoints()).then(deleteElement());
